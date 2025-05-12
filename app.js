448d723a536faa60193f5ba7d74ee770ebfe4838
Vera Fesianava <verafes@gmail.com>
2025-05-12 13:35:50 -0700

    Add auth middleware

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./db/connect');
-const storiesRouter = require('./routes/stories');
-const authRouter = require('./routes/auth');
+const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
+
+const storiesRouter = require("./routes/stories");
+const authRouter = require("./routes/auth");
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('Stories api');
+app.get("/", (req, res) => {
+  res.send("Stories api");
 });
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/stories', storiesRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/stories", authenticateUser, storiesRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
