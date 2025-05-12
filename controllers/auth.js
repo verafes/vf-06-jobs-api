31e9c46dd11e893a7c4da8f20dc68af8886484b8
Vera Fesianava <verafes@gmail.com>
2025-05-12 13:19:33 -0700

    Add token generation logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,10 @@ const { BadRequestError } = require("../errors");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.CREATED)
+    .json({ user: { name: user.getName() }, token });
 };
 
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -34,5 +35,19 @@ UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+UserSchema.methods.getName = function () {
+  return this.name;
+};
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+};
 
 module.exports = mongoose.model("User", UserSchema)
