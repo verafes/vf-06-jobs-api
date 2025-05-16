73e4edb8f4697ed279762ab4bed33b535d79bcfc
Vera Fesianava <verafes@gmail.com>
2025-05-16 16:34:10 -0700

    add security packages

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
 
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
@@ -15,9 +21,21 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
+app.set("trust proxy", 1);
+
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.get("/", (req, res) => {
+  console.log("Client IP:", req.ip);
   res.send("Stories api");
 });
 
